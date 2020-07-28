1ad2f5c217d65187d62bf43a74ce02c1beb11017
aarushi-nema <aarushi.nema@gmail.com>
2020-07-28 14:29:01 +0530

    ROI part 2

---
+++ ROI2.py
@@ -0,0 +1,16 @@
+import cv2
+dispW=640
+dispH=480
+flip=2
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam=cv2.VideoCapture(camSet)
+while True:
+    ret, frame= cam.read()
+    roi=frame[50:250, 200:400].copy()
+    frame[50:250, 200:400]= [255,255,255]
+    cv2.imshow('nanoCam', frame)
+    cv2.imshow('ROI', roi)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
