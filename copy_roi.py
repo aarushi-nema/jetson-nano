9f5933a60eca4e9838e9e7ae27ed1d822e88df77
aarushi-nema <aarushi.nema@gmail.com>
2020-07-30 09:15:27 +0530

    copy roi

---
+++ copy_roi.py
@@ -0,0 +1,34 @@
+import cv2
+goFlag=0
+def mouse_click(event, x, y, flags, params):
+    global x1,y1,x2,y2
+    global goFlag
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x1=x
+        y1=y
+        goFlag=0
+    if event == cv2.EVENT_LBUTTONUP:
+        x2=x
+        y2=y
+        goFlag=1
+
+cv2.namedWindow('nanoCam')
+cv2.setMouseCallback('nanoCam', mouse_click)
+
+dispW=640
+dispH=480
+flip=2
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam=cv2.VideoCapture(camSet)
+
+while True:
+    ret, frame = cam.read()
+    if goFlag==1:
+        frame=cv2.rectangle(frame, (x1,y1),(x2,y2),(0,0,0),3)
+        roi=frame[y1:y2, x1:x2]
+        cv2.imshow('Copy', roi)
+    cv2.imshow('nanoCam', frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
